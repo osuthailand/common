2d6b124330e96482916c142aebdb715cad9712b7
Sunpy <sunpy@osufx.com>
2018-10-09 10:44:33 +0200

    Store unranked-mods bitmask if not set on use

--- ripple/scoreUtils.py
+++ ripple/scoreUtils.py
@@ -1,7 +1,6 @@
 from common.constants import mods
 from objects import glob
 
-
 def isRankable(m):
 	"""
 	Checks if `m` contains unranked mods
@@ -9,6 +8,12 @@ def isRankable(m):
 	:param m: mods enum
 	:return: True if there are no unranked mods in `m`, else False
 	"""
+	# I am a wizard.... feel free to make sense of this and do a better job (merge req are welcome)
+	if "_unranked-mods" not in glob.conf.extra:
+		glob.conf.extra["_unranked-mods"] = sum([getattr(mods, key) for key, value in
+												glob.conf.extra["common"]["rankable-mods"].items() if not value
+												]) # Store the unranked mods mask into glob
+
 	# I know bitmasks... so get that old trash out of here ktnxbye
 	return m & ~glob.conf.extra["_unranked-mods"] == m and m & 8320 != 8320
 
